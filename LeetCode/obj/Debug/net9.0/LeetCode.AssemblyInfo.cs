dc13b5662632a3293aff10b4958b901937db6aa7
Raziel Arias <raziel.arias1991@gmail.com>
2025-06-28 00:01:59 -0400

    Adding a few of easy problems.

--- LeetCode/obj/Debug/net9.0/LeetCode.AssemblyInfo.cs
+++ LeetCode/obj/Debug/net9.0/LeetCode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efc879ce7ae084db25bffbe380d3cf0041fd4d38")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
