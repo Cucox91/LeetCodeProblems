b80069ff261001545e9f9ac2e2fd97cab7c5fd97
Raziel Arias <raziel.arias1991@gmail.com>
2025-07-20 11:36:47 -0400

    A few Medium Problems Solved

--- LeetCode/Utils/TreeNode.cs
+++ LeetCode/Utils/TreeNode.cs
@@ -1,25 +1,30 @@
 namespace LeetCode;
 
-public class TreeNode
+public class Node
 {
     public int val { get; set; }
 
-    public TreeNode? left { get; set; }
-    public TreeNode? right { get; set; }
-    public TreeNode? Parent { get; set; }
+    public Node? left { get; set; }
+    public Node? right { get; set; }
+    public Node? parent { get; set; }
 
-    public TreeNode(int val, TreeNode? left, TreeNode? right)
+    public Node()
+    {
+        
+    }
+
+    public Node(int val, Node? left, Node? right)
     {
         this.val = val;
         this.left = left;
         this.right = right;
     }
 
-    public TreeNode(int val, TreeNode? left, TreeNode? right, TreeNode? parent)
+    public Node(int val, Node? left, Node? right, Node? parent)
     {
         this.val = val;
         this.left = left;
         this.right = right;
-        this.Parent = parent;
+        this.parent = parent;
     }
 }
--- LeetCode/obj/Debug/net9.0/LeetCode.AssemblyInfo.cs
+++ LeetCode/obj/Debug/net9.0/LeetCode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc616094da442ee1e67de3681354a52580eaf265")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57aa655fd6a7414384903a3fab9df4540dee0263")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
